e5f2947d899739764ef01161efa8d76f4cd44978
Jai Parasher <jaiparasher30@gmail.com>
2024-05-06 16:46:50 +0530

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import morgan from "morgan";
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
-})); 
+}));
 
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ extended: true , limit: "50mb"}));
