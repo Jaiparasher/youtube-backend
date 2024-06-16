2bdf60b3be6efab0fe21c6e544d3f9712829ee7b
Jai Parasher <jaiparasher30@gmail.com>
2024-06-16 12:52:37 +0530

    fixed avatar uploading bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,23 +54,17 @@ export const registerUser = asyncHandler( async ( req, res )=> {
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-
-    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
-        coverImageLocalPath = req.files?.coverImage[0]?.path;
-        console.log(coverImageLocalPath);
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
     }
 
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is required")
-    }
+    if (!avatarLocalPath) throw new ApiError(400, "Avatar file is required")
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-    
+    const avatar = await uploadOnCloudinary(avatarLocalPath).catch((error) => console.log(error))
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar){
-        throw new ApiError(400,"Avatar file is required")
-    }
+    // console.log(avatar);null
+    if (!avatar) throw new ApiError(400, "Avatar file is required!!!.")
 
     const user = await User.create({
         fullName,
