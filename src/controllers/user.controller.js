f573d6163e2097dc9bdefa72bea0683e68cf6fd2
Jai Parasher <jaiparasher30@gmail.com>
2024-06-19 19:18:13 +0530

    checking

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,6 +34,7 @@ export const registerUser = asyncHandler( async ( req, res )=> {
     //return res
 
     const {fullName, email, username, password } = req.body;
+    console.log(req.files)
 
     if([fullName, email, username, password].some((field) => field?.trim() === "")){
         throw new ApiError(400,"All  fields are required"); 
@@ -51,8 +52,6 @@ export const registerUser = asyncHandler( async ( req, res )=> {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    console.log(req.files.coverImage[0].path);
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
