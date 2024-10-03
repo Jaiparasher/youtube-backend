23bb53c12eea8cd65b9c696c53b4cb31948c0a21
Jai Parasher <jaiparasher30@gmail.com>
2024-10-03 15:55:17 +0530

    fixed dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -88,7 +88,7 @@ const getChannelStats = asyncHandler(async (req, res) => {
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
     const { userId } = req.params;
-    console.log(req.params);
+    console.log(userId);
     
     try {
     const allVideos = await Video.find({
