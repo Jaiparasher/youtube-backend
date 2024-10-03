e9c331d9061a4d6beac11585067f0d6dd34f5ae0
Jai Parasher <jaiparasher30@gmail.com>
2024-10-03 14:12:21 +0530

    Remove duplicate body parser middleware configurations.

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ app.use(cors({
 
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ extended: true , limit: "50mb"}));
-app.use(bodyParser.json({limit:"50mb"}));
-app.use(bodyParser.urlencoded({extended: true, limit: "50mb"}));
 app.use(express.static("public"))
 app.use(cookieParser());
 app.use(morgan("dev")); 
